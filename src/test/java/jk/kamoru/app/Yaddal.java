4ad09160e58c25763661e6c351d4258fa818fd96
kamoru <kamoru@kamoru-home>
2018-05-13 18:59:16 +0900

    add

---
+++ src/test/java/jk/kamoru/app/Yaddal.java
@@ -0,0 +1,16 @@
+package jk.kamoru.app;
+
+import org.apache.commons.io.FileUtils;
+
+import jk.kamoru.tool.web.image.PageImageDownloader;
+
+public class Yaddal {
+
+	public static void main(String[] args) {
+		PageImageDownloader pageDownloader = new PageImageDownloader(
+				"https://www.yaddal.tv/bbs/board.php?bo_table=ys_01&wr_id=27448", "E:\\yaddal");
+		pageDownloader.setMinimumImageSize(50 * FileUtils.ONE_KB);
+		pageDownloader.download();
+	}
+
+}
